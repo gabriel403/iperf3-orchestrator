cf3ea51b3d0e94e1c566390ef6890eb3fba02f75
Gabriel Baker <gabriel.baker@dunelm.com>
2025-12-19 16:32:42 +0000

    fix: add Request/Response polyfills for Jest tests
    - Add whatwg-fetch to polyfill Request/Response in jsdom environment
    - Fixes 'Request is not defined' errors in CI
    - Tests now run successfully (19/24 passing, remaining are test logic issues)

--- server/jest.setup.js
+++ server/jest.setup.js
@@ -1,2 +1,6 @@
 // Learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom'
+require('@testing-library/jest-dom')
+
+// Polyfill fetch API (includes Request, Response, Headers)
+// This works better with jsdom than undici
+require('whatwg-fetch')
